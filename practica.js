ea915ea1c75812610a3580e0cf99f61c5c7d1e6a
a20140118 <131031295+a20140118@users.noreply.github.com>
2023-09-25 13:27:49 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,20 @@
+var nom=parseInt(prompt("Ingresa tu nombre"));
+document.write(edad)
+var numc=0
+var numi=0
+alert(document.write("Bienvenido/a al cuestionario sobre las Olimpíadas"));
+pre1=prompt("¿Cuándo se realizará la inauguración de las Olimpiadas?\nA:Lunes 02 de octubre\nB:Viernes 29 de setiembre\nC:Jueves 28 de setiembre");
+if (res1=="A"){
+    res1=0
+}
+else if(res1=="B"){
+     res1=25
+}
+else if(res1=="C"){
+    res1=0
+}
+else{
+    alert(nom,"No es una opcion validad")
+}
+
+
